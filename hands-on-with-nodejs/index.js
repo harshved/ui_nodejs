3c49ad93a7fd3827c65c5be3c429fc76417dfe48
harshved <harshved3@gmail.com>
2021-12-31 09:11:16 +0530

    5 Program Completed

--- hands-on-with-nodejs/index.js
+++ hands-on-with-nodejs/index.js
@@ -34,3 +34,4 @@ var server = http.createServer(function(req, res){
 
 console.log("Server is started");
 */
+ 
--- hands-on-with-nodejs/second.js
+++ hands-on-with-nodejs/second.js
@@ -4,4 +4,4 @@ var imp = require("./index.js");
 console.log("First Func : Addition is = ", imp.add(20,30));
 console.log("Second Func : name is = ", imp.name("Harsh Ved"));
 console.log("Third Func : Multiply is = ", imp.multiply(20,30));
-*/
+*/ 
